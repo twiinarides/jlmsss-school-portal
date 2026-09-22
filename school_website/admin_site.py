81e77dd29b98e17752228fe42b7cbc09ec0f3214
twiinarides <twiinarides@gmail.com>
2026-09-22 21:23:52 +0300

    chore: add school_website/admin_site.py

---
+++ school_website/admin_site.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Customize Admin Site
+admin.site.site_header = "School Website Administration"
+admin.site.site_title = "School Admin"
+admin.site.index_title = "Welcome to School Administration Panel"
+
