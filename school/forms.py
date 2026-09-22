c44390ea6ad556d65c2a43ea1ee4cb7d27d9ef18
twiinarides <twiinarides@gmail.com>
2026-09-22 21:22:22 +0300

    chore: add school/forms.py

---
+++ school/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import ContactMessage
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ['name', 'email', 'phone', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your Name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your Email'
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your Phone (Optional)'
+            }),
+            'subject': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Subject'
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+                'placeholder': 'Your Message'
+            }),
+        }
+
